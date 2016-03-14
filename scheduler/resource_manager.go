28874ab99e964afb5c6f36fa33ced7206ed4167c
liubin8 <liubin8@staff.weibo.com>
2016-03-14 18:18:20 +0800

    finish resource manager api

--- scheduler/resource_manager.go
+++ scheduler/resource_manager.go
@@ -17,7 +17,7 @@ func NewResourceManager() (*ResourceManager, error) {
 	}, nil
 }
 
-func (this *ResourceManager) AddMeta(meta *NodeMeta) error {
+func (this *ResourceManager) AddMeta(meta *NodeMeta) (string, error) {
 	node := &Node{
 		NodeId:    fmt.Sprintf("%s-random-something-else", meta.IP),
 		Used:      false,
@@ -27,17 +27,37 @@ func (this *ResourceManager) AddMeta(meta *NodeMeta) error {
 	this.allNodes = append(this.allNodes, node)
 	this.freeNodes = append(this.freeNodes, node)
 	// TODO store
-	return nil
+	return node.NodeId, nil
 }
 
 func (this *ResourceManager) ModifyMeta(nodeId string, meta *NodeMeta) error {
-	// TODO modify
-	// TODO store
-	return nil
+	for _, node := range this.allNodes {
+		if node.NodeId == nodeId {
+			node.Meta = meta
+			// TODO store
+			return nil
+		}
+	}
+	return fmt.Errorf("No nodeId: %d", nodeId)
 }
 
 func (this *ResourceManager) RemoveNode(nodeId string) error {
-	// TODO remove
+	idx := -1
+	for i, node := range this.allNodes {
+		if node.NodeId == nodeId {
+			idx = i
+		}
+	}
+	if idx < 0 {
+		return fmt.Errorf("No nodeId: %d", nodeId)
+	}
+
+	tNodes := this.allNodes
+	this.allNodes = tNodes[:idx]
+	if idx+1 < len(tNodes) {
+		this.allNodes = append(this.allNodes, tNodes[idx+1:]...)
+	}
+
 	// TODO store
 	return nil
 }
